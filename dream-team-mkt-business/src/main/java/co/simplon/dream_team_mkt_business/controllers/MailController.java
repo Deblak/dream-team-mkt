68b73721afd0bfc37af5388fe18e583c0f06d094
louis frederic schefer <lf.schefer@gmail.com>
2024-08-28 20:40:56 +0200

    update mail sender

--- dream-team-mkt-business/src/main/java/co/simplon/dream_team_mkt_business/controllers/MailController.java
+++ dream-team-mkt-business/src/main/java/co/simplon/dream_team_mkt_business/controllers/MailController.java
@@ -2,10 +2,12 @@ package co.simplon.dream_team_mkt_business.controllers;
 
 import java.util.Map;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import co.simplon.dream_team_mkt_business.entities.ContactForm;
@@ -24,6 +26,7 @@ public class MailController {
 	}
 
 	@PostMapping("/send")
+	@ResponseStatus(code = HttpStatus.ACCEPTED)
 	public void sendContactMail(@RequestBody Map<String, @Valid ContactForm> inputs) {
 		service.sendContactMail(inputs.get("data"));
 	}
