7391dc16d22c80071abf05618ed9df7365c8066b
Deblak <deblak@mailo.com>
2024-08-28 16:51:32 +0200

    first commit for business

---
+++ dream-team-mkt-business/src/main/java/co/simplon/dream_team_mkt_business/DreamTeamBusinessApplication.java
@@ -0,0 +1,14 @@
+package co.simplon.dream_team_mkt_business;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamTeamBusinessApplication {
+
+    public static void main(String[] args) {
+	SpringApplication.run(DreamTeamBusinessApplication.class, args);
+	System.out.println("Hello world !");
+    }
+
+}
