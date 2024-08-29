285ed9952f6c977815dcf430cc4cdb32b1530c73
dumele françois <fjldumele@gmail.com>
2024-08-29 17:29:04 +0200

    add panel <p> on panel.vue

--- dream-team-mkt-presentation/src/components/panel/panel.vue
+++ dream-team-mkt-presentation/src/components/panel/panel.vue
@@ -1,9 +1,9 @@
 <script setup></script>
 <template>
     <section class="container">
-        <span>Multiline message is:</span>
+        <span> <p> Multiline message is: </p> </span>
         <p style="white-space: pre-line;">{{ message }}</p>
-        <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+        <label for="exampleFormControlTextarea1" class="form-label"> <p>Example textarea</p> </label>
 
         <div class="row row-cols-lg-2">
             <div class="mb-3">
