251eda1ef3a181c50ee77e5b7344ab05759314dd
Deblak <deblak@mailo.com>
2024-08-29 15:56:14 +0200

    Get list Offer and DreamTeamId entities

--- dream-team-mkt-business/src/main/java/co/simplon/dream_team_mkt_business/services/DreamTeamIdService.java
+++ dream-team-mkt-business/src/main/java/co/simplon/dream_team_mkt_business/services/DreamTeamIdService.java
@@ -1,9 +1,7 @@
 package co.simplon.dream_team_mkt_business.services;
 
-import java.util.List;
-
-import co.simplon.dream_team_mkt_business.entities.DreamTeamId;
+import co.simplon.dream_team_mkt_business.dtos.DreamTeamIdDto;
 
 public interface DreamTeamIdService {
-    List<DreamTeamId> getAll();
+    DreamTeamIdDto getAll();
 }
--- dream-team-mkt-business/src/main/java/co/simplon/dream_team_mkt_business/services/implementation/DreamTeamIdServiceImpl.java
+++ dream-team-mkt-business/src/main/java/co/simplon/dream_team_mkt_business/services/implementation/DreamTeamIdServiceImpl.java
@@ -1,10 +1,9 @@
 package co.simplon.dream_team_mkt_business.services.implementation;
 
-import java.util.List;
-
 import org.springframework.stereotype.Service;
 
-import co.simplon.dream_team_mkt_business.entities.DreamTeamId;
+import co.simplon.dream_team_mkt_business.dtos.DreamTeamIdDto;
+import co.simplon.dream_team_mkt_business.mapper.DreamTeamIdMapper;
 import co.simplon.dream_team_mkt_business.repositories.DreamTeamIdRepository;
 import co.simplon.dream_team_mkt_business.services.DreamTeamIdService;
 
@@ -13,13 +12,12 @@ public class DreamTeamIdServiceImpl implements DreamTeamIdService {
     private final DreamTeamIdRepository repository;
 
     public DreamTeamIdServiceImpl(DreamTeamIdRepository repository) {
-	super();
 	this.repository = repository;
     }
 
     @Override
-    public List<DreamTeamId> getAll() {
-	return repository.findAll();
+    public DreamTeamIdDto getAll() {
+	return DreamTeamIdMapper.entityToDto(repository.findAll().getLast());
     }
 
 }
