6b4d807b74da70fb180dcfae191eebc7d9903c99
louis frederic schefer <lf.schefer@gmail.com>
2024-08-30 05:34:35 +0200

    price update refacto

--- dream-team-mkt-presentation/src/components/PricingsProduct.vue
+++ dream-team-mkt-presentation/src/components/PricingsProduct.vue
@@ -7,57 +7,41 @@ const props = defineProps({
 })
 
 const datas = ref([]);
-const formatedDatas = ref([]);
 const isInEdition = ref(false);
-
-function syncFormatedData() {
-  formatedDatas.value = datas.value.map( data => {
-      return {
-        nameOfferEn: data.nameOfferEn,
-        nameOfferFr: data.nameOfferFr,
-        priceOfferFr: data.priceOfferFr,
-        priceOfferEn: data.priceOfferEn,
-        planOfferFr: data.planOfferFr.split(","),
-        planOfferEn: data.planOfferEn.split(","),
-        callToActionFr: data.callToActionFr,
-        callToActionEn: data.callToActionEn
-      }
-    });
-}
 
 onMounted( async () => {
   datas.value = await PricingProductService.fetchData();
-  syncFormatedData()
 })
 
 function toggleEdition() {
-  //isInEdition.value = true;
-  isInEdition.value = !isInEdition.value;
-
+  isInEdition.value = true;
 }
 
 async function saveChange() {
   await PricingProductService.updateData(datas.value);
   datas.value = await PricingProductService.fetchData();
-  syncFormatedData();
   isInEdition.value = false;
+}
+
+function splitPlanOffer(params) {
+  return params.split(',')
 }
 
 </script>
 
 <template>
     <section class="container ">
+        <label v-if="props.isEditable && isInEdition">English</label>
         <div class="row row-cols-lg-3 bg-info-subtle">
-          <div v-for="formatedData in formatedDatas" :key="formatedData.nameOfferEn" class="p-2 text-center">
-            <h3 class="h3">{{formatedData.nameOfferEn}}</h3>
-            <h4 class="h3">{{formatedData.priceOfferEn}}</h4>
+          <div v-for="data in datas" :key="data.nameOfferEn" class="p-2 text-center">
+            <h3 class="h3">{{data.nameOfferEn}}</h3>
+            <h4 class="h3">{{data.priceOfferEn}}</h4>
             <ul  class="list-unstyled">
-                <li v-for="value in formatedData.planOfferEn" :key="value" >&bull;&#160;{{ value }}</li>
-                <!-- <li v-for="value in formatedData.planOfferEn" :key="value" >&bull;&#160;{{ value }}</li> -->
+                <li v-for="value in splitPlanOffer(data.planOfferEn)" :key="value" >&bull;&#160;{{ value }}</li>
             </ul>
         </div>
       </div>
-      <label >English</label>
+      <label v-if="props.isEditable && isInEdition">English</label>
       <div v-if="isInEdition" class="row row-cols-lg-3 bg-info-subtle">
         <div v-for="data in datas" :key="data.nameOfferEn" class="p-2 text-center">
           <input class="h3" v-model="data.nameOfferEn" type="text" />
@@ -65,7 +49,7 @@ async function saveChange() {
           <textarea type="text" v-model="data.planOfferEn"></textarea>
         </div>
       </div>
-      <label >Français</label>
+      <label v-if="props.isEditable && isInEdition">Français</label>
       <div v-if="isInEdition" class="row row-cols-lg-3 bg-info-subtle">
           <div v-for="data in datas" :key="data.nameOfferFr" class="p-2 text-center">
             <input class="h3" v-model="data.nameOfferFr" type="text" />
