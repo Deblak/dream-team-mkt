f2d713bbb3ba996b7cfc8115e9a1736a6d8f0c66
Léonard <fjldumele@gmail.com>
2024-08-30 15:33:12 +0200

    try to fix the navbar

--- dream-team-mkt-presentation/src/components/HeaderComponent.vue
+++ dream-team-mkt-presentation/src/components/HeaderComponent.vue
@@ -1,50 +1,46 @@
-
 <script setupt></script>
 
 <template>
-    <header>
-      <nav class="fixed-top navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid text-bg-primary">
-      <div class="nav-item d-flex justify-content-between">
-            <router-link to="/" class="btn btn-primary">Home</router-link>
-          </div>
-      <div class="nav-item d-flex justify-content-between">
-            <router-link to="/edit" class="btn btn-primary">Edit page</router-link>
-          </div>
-  
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-  
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
+  <header>
+    <nav class="fixed-top navbar navbar-expand-lg bg-body-tertiary d-flex justify-content-between">
+      <div class="container-fluid text-bg-primary">
+        <div class="nav-item d-flex">
+          <router-link to="/" class="btn btn-primary">Home</router-link>
+        </div>
+
+        <div class="collapse navbar-collapse d-flex justify-content-center">
+          <a class="navbar-brand" href="#">DreamTeam</a>
+        </div>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-auto">
+
+          <!-- <div class="nav-item d-flex justify-content-between">
+                <router-link to="/edit" class="btn btn-primary">Edit page</router-link>
+              </div> -->
+              
           <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" id="navbarSupportedContent"
+            >
+              Menu
             </a>
-  
-            <ul class="dropdown-menu">
+            <ul class="dropdown-menu dropdown-menu-end" aria-controls="navbarSupportedContent">
+              <li><router-link  to="/edit" href="#" class="btn btn-primary dropdown-item">Edit page</router-link></li>
               <li><a class="dropdown-item" href="#">Action</a></li>
               <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><hr class="dropdown-divider"></li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
               <li><a class="dropdown-item" href="#">Something else here</a></li>
             </ul>
           </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-          </li>
         </ul>
-        <a class="navbar-brand" href="#">DreamTeam</a>
-        <form class="d-flex" role="search">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
       </div>
-    </div>
-  </nav>
-    </header>
-    <RouterView />
-  </template>
+    </nav>
+  </header>
+  <RouterView />
+</template>
