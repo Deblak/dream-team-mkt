416697e4ba5fca55fdf08a230f479e999739e546
Deblak <deblak@mailo.com>
2024-08-30 14:15:46 +0200

    Updates form style

--- dream-team-mkt-data/Scripts/schema.ddl.sql
+++ dream-team-mkt-data/Scripts/schema.ddl.sql
@@ -3,25 +3,25 @@ DROP TABLE IF EXISTS t_dream_team_identity;
 
 CREATE TABLE t_offers(
 	id_offer bigint GENERATED ALWAYS AS IDENTITY,
-	name_offer_fr VARCHAR(50) NOT null,
-	name_offer_en VARCHAR(50) NOT null,
-	price_offer_fr VARCHAR(200) NOT null,
-	price_offer_en VARCHAR(50) NOT null,
-   plan_offer_fr VARCHAR(500) NOT null,
-   plan_offer_en VARCHAR(500) NOT null,
-   call_to_action_fr VARCHAR(200) NOT null,
-   call_to_action_en VARCHAR(200) NOT null,
+	name_offer_fr VARCHAR(50) NOT null, --data
+	name_offer_en VARCHAR(50) NOT null, --data
+	price_offer_fr VARCHAR(100) NOT null,
+	price_offer_en VARCHAR(100) NOT null,
+   plan_offer_fr VARCHAR(1000) NOT null,
+   plan_offer_en VARCHAR(1000) NOT null,
+   call_to_action_fr VARCHAR(200) NOT null, --data
+   call_to_action_en VARCHAR(200) NOT null, --data
    CONSTRAINT t_offers_pkey PRIMARY KEY (id_offer)
 );
 
 CREATE TABLE t_dream_team_identity(
 	id_identity bigint GENERATED ALWAYS AS IDENTITY,
 	picture VARCHAR(1000) NOT null,
-	slogan_fr VARCHAR(200) NOT NULL,
-	slogan_en VARCHAR(200) NOT null,
-	panel_1_desc_fr VARCHAR(1000) NOT null,
-	panel_1_desc_en VARCHAR(1000) NOT null,
-	panel_2_desc_fr VARCHAR(1000) NOT null,
-	panel_2_desc_en VARCHAR(1000) NOT null,
+	slogan_fr VARCHAR(300) NOT NULL,
+	slogan_en VARCHAR(300) NOT null,
+	panel_1_desc_fr VARCHAR(2000) NOT null,
+	panel_1_desc_en VARCHAR(2000) NOT null,
+	panel_2_desc_fr VARCHAR(2000) NOT null,
+	panel_2_desc_en VARCHAR(2000) NOT null,
    CONSTRAINT t_dream_team_identity_pkey PRIMARY KEY (id_identity)
 );
--- dream-team-mkt-presentation/src/components/ContactForm.vue
+++ dream-team-mkt-presentation/src/components/ContactForm.vue
@@ -25,15 +25,15 @@ function inputIsValid(inputs) {
   inputFormDataIsValid.value.lastName = true;
   inputFormDataIsValid.value.emailPhoneNumber = true;
   inputFormDataIsValid.value.message = true;
-  if (inputs.value.corporateName === '' || inputs.value.corporateName.trim().length > 200 ) {
+  if (inputs.value.corporateName === '' || inputs.value.corporateName.trim().length > 200) {
     inputFormDataIsValid.value.corporateName = false;
     isValid = false;
   }
-  if (inputs.value.firstName === '' || inputs.value.firstName.trim().length > 200 ) {
+  if (inputs.value.firstName === '' || inputs.value.firstName.trim().length > 200) {
     inputFormDataIsValid.value.firstName = false;
     isValid = false;
   }
-  if (inputs.value.lastName === '' || inputs.value.lastName.trim().length > 200 ) {
+  if (inputs.value.lastName === '' || inputs.value.lastName.trim().length > 200) {
     inputFormDataIsValid.value.lastName = false;
     isValid = false;
   }
@@ -66,66 +66,84 @@ function submitData() {
 
 <template>
   <div class="container bg-light border rounded-2">
-  <form novalidate class="p4">
-    <div class="contact-form-container col-12 py-3 ">
-      <h1 class="text-center m-3">Contact form</h1>
+    <form novalidate class="p4 d-flex">
+      <div class="contact-form-container col-12 py-3 ">
+        <h1 class="text-center m-3">Contact form</h1>
         <div class="m-3 row">
           <div class="field mx-3 col-10 col-lg-4">
-            <label for="corporateName" class="form-label">Corporate names<span class="text-danger"><span class="text-danger">*</span></span></label>
-            <input v-model="inputFormData.corporateName" type="text" class="form-control" v-bind:class="{borderIsRed: !inputFormDataIsValid.corporateName}" id="corporateName" placeholder="Example input placeholder" required>
-            <span v-if="!inputFormDataIsValid.corporateName" class="feedback fst-italic text-danger">Corporate name can't be blank, max 200 characters !</span>
+            <label for="corporateName" class="form-label fs-5">Corporate names<span class="text-danger"><span
+                  class="text-danger">*</span></span></label>
+            <input v-model="inputFormData.corporateName" type="text" class="form-control"
+              v-bind:class="{ borderIsRed: !inputFormDataIsValid.corporateName }" id="corporateName"
+              placeholder="Example input placeholder" required>
+            <span v-if="!inputFormDataIsValid.corporateName" class="feedback fst-italic text-danger">Corporate name
+              can't be blank, max 200 characters !</span>
           </div>
         </div>
         <div class="row row-cols-lg-2 m-3">
           <div class="col-10 col-lg-4">
             <div class="field mx-3">
-              <label for="firstName" class="form-label">First name<span class="text-danger">*</span></label>
-              <input v-model="inputFormData.firstName" type="text" class="form-control" v-bind:class="{borderIsRed: !inputFormDataIsValid.firstName}" id="firstName" placeholder="Another input placeholder" required>
-              <span v-if="!inputFormDataIsValid.firstName" class="feedback fst-italic text-danger">First name can't be blank, max 200 characters !</span>
+              <label for="firstName" class="form-label fs-5">First name<span class="text-danger">*</span></label>
+              <input v-model="inputFormData.firstName" type="text" class="form-control"
+                v-bind:class="{ borderIsRed: !inputFormDataIsValid.firstName }" id="firstName"
+                placeholder="Another input placeholder" required>
+              <span v-if="!inputFormDataIsValid.firstName" class="feedback fst-italic text-danger">First name can't be
+                blank, max 200 characters !</span>
             </div>
           </div>
           <div class="col-10 col-lg-4">
             <div class="field mx-3">
-              <label for="lastName" class="form-label">Last name<span class="text-danger">*</span></label>
-              <input v-model="inputFormData.lastName" type="text" class="form-control" v-bind:class="{borderIsRed: !inputFormDataIsValid.lastName}" id="lastName" placeholder="Another input placeholder" required>
-              <span v-if="!inputFormDataIsValid.lastName" class="feedback fst-italic text-danger">Last name can't be blank, max 200 characters !</span>
+              <label for="lastName" class="form-label fs-5">Last name<span class="text-danger">*</span></label>
+              <input v-model="inputFormData.lastName" type="text" class="form-control"
+                v-bind:class="{ borderIsRed: !inputFormDataIsValid.lastName }" id="lastName"
+                placeholder="Another input placeholder" required>
+              <span v-if="!inputFormDataIsValid.lastName" class="feedback fst-italic text-danger">Last name can't be
+                blank, max 200 characters !</span>
+            </div>
           </div>
         </div>
-      </div>
-      <div class="row row-cols-lg-2 m-3">
-        <div class="col-10 col-lg-4">
-          <div class="field mx-3 ">
-            <label for="email" class="form-label">Email<span class="text-danger">*</span></label>
-            <input v-model="inputFormData.email" type="email" class="form-control" v-bind:class="{borderIsRed: !inputFormDataIsValid.emailPhoneNumber}" id="email" placeholder="Another input placeholder">
-            <span v-if="!inputFormDataIsValid.emailPhoneNumber" class="feedback fst-italic text-danger">Email or Phone number can't be blank !</span>
+        <div class="row row-cols-lg-2 m-3">
+          <div class="col-10 col-lg-4">
+            <div class="field mx-3 ">
+              <label for="email" class="form-label fs-5">Email<span class="text-danger">*</span></label>
+              <input v-model="inputFormData.email" type="email" class="form-control"
+                v-bind:class="{ borderIsRed: !inputFormDataIsValid.emailPhoneNumber }" id="email"
+                placeholder="Another input placeholder">
+              <span v-if="!inputFormDataIsValid.emailPhoneNumber" class="feedback fst-italic text-danger">Email or Phone
+                number can't be blank !</span>
+            </div>
+          </div>
+          <div class="col-10 col-lg-4">
+            <div class="field mx-3">
+              <label for="phoneNumber" class="form-label fs-5">Phone number<span class="text-danger">*</span></label>
+              <input v-model="inputFormData.phoneNumber" type="tel" class="form-control"
+                v-bind:class="{ borderIsRed: !inputFormDataIsValid.emailPhoneNumber }" id="phoneNumber"
+                placeholder="Another input placeholder">
+              <span v-if="!inputFormDataIsValid.emailPhoneNumber" class="feedback fst-italic text-danger">Email or Phone
+                number can't be blank !</span>
+            </div>
           </div>
         </div>
-        <div class="col-10 col-lg-4">
-          <div class="field mx-3">
-            <label for="phoneNumber" class="form-label">Phone number<span class="text-danger">*</span></label>
-            <input v-model="inputFormData.phoneNumber" type="tel" class="form-control" v-bind:class="{borderIsRed: !inputFormDataIsValid.emailPhoneNumber}" id="phoneNumber" placeholder="Another input placeholder">
-            <span v-if="!inputFormDataIsValid.emailPhoneNumber" class="feedback fst-italic text-danger">Email or Phone number can't be blank !</span>
+        <div class="m-3 row">
+          <div class="field mx-3 col-10">
+            <label for="message" class="form-label fs-5">Message<span class="text-danger">*</span></label>
+            <textarea v-model="inputFormData.message" class="form-control" id="message"
+              v-bind:class="{ borderIsRed: !inputFormDataIsValid.message }" rows="3" required></textarea>
+            <span v-if="!inputFormDataIsValid.message" class="feedback fst-italic text-danger">Message can't be blank,
+              max 2000 characters !</span>
           </div>
         </div>
-      </div>
-      <div class="m-3 row">
-        <div class="field mx-3 col-10">
-          <label for="message" class="form-label">Message</label>
-          <textarea v-model="inputFormData.message" class="form-control" id="message" v-bind:class="{borderIsRed: !inputFormDataIsValid.message}" rows="3" required></textarea>
-          <span v-if="!inputFormDataIsValid.message" class="feedback fst-italic text-danger">Message can't be blank, max 2000 characters !</span>
+        <div class="m-4 row">
+          <button v-on:click="submitData" class="btn btn-primary mt-4 ">SEND
+          </button>
         </div>
       </div>
-      <div class="m-4 row">
-        <button v-on:click="submitData" class="btn btn-primary mx-4 mt-4 col-10 col-xl-4">Submit
-      </button>
-    </div>
-    </div>
-  </form>
-</div>
+    </form>
+  </div>
 </template>
 
 <style>
-.borderIsRed  {
+.borderIsRed {
   border-color: red;
 }
 </style>
