97fd6f8a55170b73215dc28f4965ea7ec30a286c
Deblak <deblak@mailo.com>
2024-08-30 11:04:05 +0200

    EditPage for the IdentityProduct in frontend

--- dream-team-mkt-presentation/src/views/EditPage.vue
+++ dream-team-mkt-presentation/src/views/EditPage.vue
@@ -4,13 +4,12 @@ import HeaderComponet from '@/components/HeaderComponent.vue';
 import IdentityProduct from '@/components/IdentityProduct.vue';
 import PricingsProduct from '@/components/PricingsProduct.vue';
 
-
 </script>
 <template>
   <main>
     <HeaderComponet/>
     <h1>Edit page</h1>
-    <IdentityProduct/>
+    <IdentityProduct :isEditable="true"/>
     <PanelComponent/>
     <PricingsProduct/>
   </main>
